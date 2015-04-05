ecad2d72b4c329f32af3dd1d38309ed388840559
Scott Smith <scottksmith95@gmail.com>
2015-04-05 15:03:52 -0700

    Added other endpoints for api

--- csharp/src/PackageTrack/PackageTrack.Web/Controllers/Api/PackagesController.cs
+++ csharp/src/PackageTrack/PackageTrack.Web/Controllers/Api/PackagesController.cs
@@ -12,7 +12,7 @@ using System.Web.Http.Description;
 using PackageTrack.Web.Data;
 using PackageTrack.Web.Models;
 
-namespace PackageTrack.Web.Controllers
+namespace PackageTrack.Web.Controllers.Api
 {
     public class PackagesController : ApiController
     {
@@ -37,6 +37,72 @@ namespace PackageTrack.Web.Controllers
             return Ok(package);
         }
 
+        // PUT: api/Packages/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPackage(int id, Package package)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != package.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(package).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PackageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Packages
+        [ResponseType(typeof(Package))]
+        public async Task<IHttpActionResult> PostPackage(Package package)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Packages.Add(package);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = package.Id }, package);
+        }
+
+        // DELETE: api/Packages/5
+        [ResponseType(typeof(Package))]
+        public async Task<IHttpActionResult> DeletePackage(int id)
+        {
+            Package package = await db.Packages.FindAsync(id);
+            if (package == null)
+            {
+                return NotFound();
+            }
+
+            db.Packages.Remove(package);
+            await db.SaveChangesAsync();
+
+            return Ok(package);
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -44,6 +110,11 @@ namespace PackageTrack.Web.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        private bool PackageExists(int id)
+        {
+            return db.Packages.Count(e => e.Id == id) > 0;
         }
     }
 }
